8b586380eb249c9eb089fec228d7ee011635674a
James Corbett <corbett8@llnl.gov>
2021-01-12 14:48:25 -0800

    Python: let users pass arguments to FluxExecutor's brokers

--- src/bindings/python/flux/job/executor.py
+++ src/bindings/python/flux/job/executor.py
@@ -12,9 +12,9 @@ import flux.job
 class WaitingThread(threading.Thread):
     """Thread that waits for jobs to complete and fulfills futures."""
 
-    def __init__(self, exit_event, future_queue, **kwargs):
+    def __init__(self, exit_event, future_queue, broker_args, broker_kwargs, **kwargs):
         super().__init__(**kwargs)
-        self.__flux_handle = flux.Flux()
+        self.__broker = flux.Flux(*broker_args, **broker_kwargs)
         self.__exit_event = exit_event
         self.__future_queue = future_queue
         self.__unrecognized_results = collections.deque()
@@ -49,7 +49,7 @@ class WaitingThread(threading.Thread):
     def _get_new_result(self):
         """Return the latest completed job from Flux, or None."""
         try:
-            return flux.job.wait(self.__flux_handle)
+            return flux.job.wait(self.__broker)
         except:  # no waitable jobs
             return None
 
@@ -73,29 +73,37 @@ class WaitingThread(threading.Thread):
 class SubmissionThread(threading.Thread):
     """Thread that, when started, submits jobspecs to Flux."""
 
-    def __init__(self, exit_event, jobspecs_to_submit, jobid_future_pairs, **kwargs):
+    def __init__(
+        self,
+        exit_event,
+        jobspecs_to_submit,
+        jobid_future_pairs,
+        broker_args,
+        broker_kwargs,
+        **kwargs,
+    ):
         super().__init__(**kwargs)
         self.__exit_event = exit_event
         self.__jobspecs_to_submit = jobspecs_to_submit
         self.__jobid_future_pairs = jobid_future_pairs
+        self.__broker = flux.Flux(*broker_args, **broker_kwargs)
 
     def run(self):
         """Loop indefinitely, submitting jobspecs and fetching jobids."""
-        broker = flux.Flux()
         while not self.__exit_event.is_set():
             while self.__jobspecs_to_submit:
                 jobspec, user_future = self.__jobspecs_to_submit.popleft()
-                flux.job.submit_async(broker, jobspec, waitable=True).then(
+                flux.job.submit_async(self.__broker, jobspec, waitable=True).then(
                     self._get_jobid_from_submission_future, user_future
                 )
             if (
-                broker.reactor_run(
-                    broker.get_reactor(), flux.constants.FLUX_REACTOR_NOWAIT
+                self.__broker.reactor_run(
+                    self.__broker.get_reactor(), flux.constants.FLUX_REACTOR_NOWAIT
                 )
                 < 0
             ):
                 msg = "reactor start failed"
-                broker.fatal_error(msg)
+                self.__broker.fatal_error(msg)
                 raise RuntimeError(msg)
 
     def _get_jobid_from_submission_future(self, submission_future, user_future):
@@ -108,26 +116,42 @@ class FluxExecutor:
     """Provides methods to submit jobs to Flux asynchronously.
 
     Heavily inspired by the ``concurrent.futures.Executor`` class.
+
+    :param thread_name_prefix: used to control the names of ``threading.Thread``
+        objects created by the executor, for easier debugging.
+    :param broker_args: positional arguments to the ``flux.Flux`` instances used by
+        the executor.
+    :param broker_args: keyword arguments to the ``flux.Flux`` instances used by
+        the executor.
     """
 
     # Used to assign unique thread names when thread_name_prefix is not supplied.
     _counter = itertools.count().__next__
 
-    def __init__(self, thread_name_prefix=""):
+    def __init__(self, thread_name_prefix="", broker_args=(), broker_kwargs={}):
         self._submission_queue = collections.deque()
         self._jobid_future_pairs = collections.deque()
         self._shutdown_event = threading.Event()
-        thread_name_prefix = (thread_name_prefix or f"{type(self).__name__}-{self._counter()}")
+        thread_name_prefix = (
+            thread_name_prefix or f"{type(self).__name__}-{self._counter()}"
+        )
         self._submission_thread = SubmissionThread(
             self._shutdown_event,
             self._submission_queue,
             self._jobid_future_pairs,
+            broker_args,
+            broker_kwargs,
             daemon=True,
-            name=(thread_name_prefix + "-submission")
+            name=(thread_name_prefix + "-submission"),
         )
         self._submission_thread.start()
         self._waiting_thread = WaitingThread(
-            self._shutdown_event, self._jobid_future_pairs, daemon=True, name=(thread_name_prefix + "-waiting")
+            self._shutdown_event,
+            self._jobid_future_pairs,
+            broker_args,
+            broker_kwargs,
+            daemon=True,
+            name=(thread_name_prefix + "-waiting"),
         )
         self._waiting_thread.start()
 
